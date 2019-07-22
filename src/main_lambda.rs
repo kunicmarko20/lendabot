6aed55c9fef0ff3cde088a40cdf2df5f0214c805
kunicmarko20 <kunicmarko20@gmail.com>
2019-07-22 21:03:14 +0100

    log request test

--- src/main_lambda.rs
+++ src/main_lambda.rs
@@ -1,15 +1,17 @@
-#[macro_use] extern crate lambda_runtime as lambda;
-
 use std::error::Error;
-use lambda_runtime::error::HandlerError;
-use serde_json::Value;
+use lambda_runtime::{error::HandlerError, Context};
+use lambda_http::{lambda, Request, Response, Body};
 
 fn main() -> Result<(), Box<dyn Error>> {
     lambda!(run);
     Ok(())
 }
 
-pub fn run(event: Value, context: lambda::Context) -> Result<(), HandlerError> {
-    //Your code here
-    Ok(())
+pub fn run(request: Request, _: Context) -> Result<Response<Body>, HandlerError> {
+    println!("{}", std::str::from_utf8(request.body().as_ref()).unwrap());
+
+    Ok(Response::builder()
+        .status(200)
+        .body("Ok".into())
+        .expect("Error while creating response."))
 }
